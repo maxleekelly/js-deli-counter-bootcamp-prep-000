c6f1d84b4911c4e02ac9d3c91b820b4c40b16c2c
Max Kelly <maxleekelly@gmail.com>
2017-12-13 18:28:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    
+    var current = `Currently serving ${katzDeliLine[0]}`;
+    katzDeliLine.shift();
+    return current;
   }
 }
