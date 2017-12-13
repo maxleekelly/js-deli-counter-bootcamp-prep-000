052a1b6d30fc4812a639b0cd1bf67ce398b8a965
Max Kelly <maxleekelly@gmail.com>
2017-12-13 18:32:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,13 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
+  var line = []
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-    
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      line.push(`${i+1}. ${katzDeliLine[i]}`);
+      
+    }
   }
 }
